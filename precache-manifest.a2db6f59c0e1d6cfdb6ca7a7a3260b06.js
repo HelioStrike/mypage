e585e552c583ff04fa87e0202e1d21ca565cf664
HelioStrike <rageofepicfury@gmail.com>
2019-04-02 19:48:29 +0530

    Updates

---
+++ precache-manifest.a2db6f59c0e1d6cfdb6ca7a7a3260b06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mypage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d0eb0fb2dd27e69d724",
+    "url": "/mypage/static/js/runtime~main.4c2b4ee8.js"
+  },
+  {
+    "revision": "8c036a76bf328f643c36",
+    "url": "/mypage/static/js/main.0cb95498.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/mypage/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8c036a76bf328f643c36",
+    "url": "/mypage/static/css/main.d2d56582.chunk.css"
+  },
+  {
+    "revision": "601488a40cf2b1e967e5db7a1069965b",
+    "url": "/mypage/index.html"
+  }
+];
